8d079c313e28c47bd2d181daaeff3a6b881e2605
Tyler Crawford <tcrawford@figure.com>
2026-04-16 13:22:32 -0400

    feat(cmd): add copy command

---
+++ cmd/copy.go
@@ -0,0 +1,35 @@
+package cmd
+
+import (
+	"github.com/opm-cli/opm/internal/output"
+	"github.com/opm-cli/opm/internal/paths"
+	"github.com/spf13/cobra"
+)
+
+var copyCmd = &cobra.Command{
+	Use:               "copy <src> <dst>",
+	Short:             "Copy a profile to a new name",
+	Args:              cobra.ExactArgs(2),
+	PersistentPreRunE: managedGuard,
+	ValidArgsFunction: profileNameCompletion,
+	SilenceUsage:      true,
+	RunE:              runCopy,
+}
+
+func init() {
+	rootCmd.AddCommand(copyCmd)
+}
+
+func runCopy(cmd *cobra.Command, args []string) error {
+	src, dst := args[0], args[1]
+	s := newStore()
+	if err := s.CopyProfile(src, dst); err != nil {
+		return err
+	}
+	dstDir := paths.ProfileDir(dst)
+	output.Success(cmd.OutOrStdout(),
+		"Copied "+output.ProfileName(src)+" → "+output.ProfileName(dst),
+		output.ShortenHome(dstDir)+"/",
+	)
+	return nil
+}
