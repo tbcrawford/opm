28d781a06c9cf3c8dcdaaf72cf9b9b8bc6118bfc
Tyler Crawford <tcrawford@figure.com>
2026-04-16 13:22:59 -0400

    feat(cmd): add path command

---
+++ cmd/path.go
@@ -0,0 +1,32 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var pathCmd = &cobra.Command{
+	Use:               "path <name>",
+	Short:             "Print the filesystem path to a profile directory",
+	Args:              cobra.ExactArgs(1),
+	PersistentPreRunE: managedGuard,
+	ValidArgsFunction: profileNameCompletion,
+	SilenceUsage:      true,
+	RunE:              runPath,
+}
+
+func init() {
+	rootCmd.AddCommand(pathCmd)
+}
+
+func runPath(cmd *cobra.Command, args []string) error {
+	name := args[0]
+	s := newStore()
+	profilePath, err := s.GetProfile(name)
+	if err != nil {
+		return err
+	}
+	fmt.Fprintln(cmd.OutOrStdout(), profilePath)
+	return nil
+}
